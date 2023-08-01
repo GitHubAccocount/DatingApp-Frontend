b638fc8079124a460a6c31bff5295d32bcb2a285
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-01 21:47:02 +0200

    feat: add new tailwind classes make the component responsive

--- src/components/loveSearch/TheHero.vue
+++ src/components/loveSearch/TheHero.vue
@@ -1,13 +1,15 @@
 <template>
   <main>
     <section class="h-svh80 px-6">
-      <div class="grid h-full grid-rows-2 rounded-md border bg-red-100 shadow-md shadow-gray-700">
+      <div
+        class="grid h-full grid-rows-2 rounded-md border bg-red-100 shadow-md shadow-gray-700 lg:grid-cols-2 lg:grid-rows-none"
+      >
         <!-- Image container -->
-        <div class="row-span-1">
+        <div class="row-span-1 lg:col-span-1">
           <photos></photos>
         </div>
         <!-- Content container -->
-        <div class="row-span-1 flex flex-col items-center pt-6">
+        <div class="row-span-1 flex flex-col items-center pt-6 lg:col-span-1 lg:justify-center">
           <h1 class="text-xl font-bold xs:text-2xl sm:text-4xl">Want to find your other half?</h1>
           <h3 class="pb-6 text-xs xs:text-base sm:text-2xl">
             Fill the form and find the
