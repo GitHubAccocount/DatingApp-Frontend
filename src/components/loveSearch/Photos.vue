722be683de6265c3a14f04a815d86ba9a609c38f
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-01 14:55:36 +0200

    fix: replace photos with lighter versions, before not able to push to online repository

--- src/components/loveSearch/Photos.vue
+++ src/components/loveSearch/Photos.vue
@@ -9,15 +9,15 @@
 <script lang="ts" setup>
 import { onBeforeUnmount, ref } from 'vue';
 
-const photo = ref<string>('assets/couple1.jpg');
+const photo = ref<string>('assets/couple1v2.jpeg');
 const currentIndex = ref<number>(0);
 
 const changePhoto = () => {
   const photos = [
-    'assets/couple1.jpg',
-    'assets/couple2.jpg',
-    'assets/couple3.jpg',
-    'assets/couple4.jpg'
+    'assets/couple1v2.jpeg',
+    'assets/couple2v2.jpeg',
+    'assets/couple3v2.jpeg',
+    'assets/couple4v2.jpeg'
   ];
   currentIndex.value = (currentIndex.value + 1) % photos.length;
   photo.value = photos[currentIndex.value];
