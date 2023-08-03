a50e90fcefeea00f4b68efae1a1b99bdbe5838b7
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-03 18:21:53 +0200

    test: add test for getQuestions.ts

---
+++ tests/unit/api/getQuestions.test.ts
@@ -0,0 +1,20 @@
+import getQuestions from '@/api/getQuestions';
+import axios from 'axios';
+
+vi.mock('axios');
+//vi.mocked - 'Type helper for TypeScript.'
+const mockedAxios = vi.mocked(axios.get);
+
+describe('getQuestios', () => {
+  beforeEach(() => {
+    mockedAxios.mockResolvedValue({ data: [{ text: 'sample text', id: 1 }] });
+  });
+  it('makes GET request to fetch questions', async () => {
+    await getQuestions();
+    expect(axios.get).toHaveBeenLastCalledWith('http://myfakeapi.com/questions');
+  });
+  it('extract questions from response', async () => {
+    const questions = await getQuestions();
+    expect(questions).toEqual([{ text: 'sample text', id: 1 }]);
+  });
+});
