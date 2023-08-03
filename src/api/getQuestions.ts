88d84df4a0608fd453dc6d2b6f22769259d99a3d
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-03 15:19:54 +0200

    feat: add types.ts creating types for api files, add the new type to getQuestions.ts

--- src/api/getQuestions.ts
+++ src/api/getQuestions.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import type { Question } from './types';
 
 const getQuestions = async () => {
   const url = 'http://localhost:3000/questions';
-  const response = await axios.get(url);
+  const response = await axios.get<Question>(url);
   return response.data;
 };
 
