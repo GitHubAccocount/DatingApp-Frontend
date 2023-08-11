e2b75026f33cc8480e6a67324cd94841c37dde74
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-11 14:29:19 +0200

    test: add test for TheFormAlreadyMade.vue

---
+++ src/components/form/TheFormAlreadyMade.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex w-full flex-col items-center">
+    <h1 class="pb-3 text-center text-3xl font-bold">
+      It seems that the test has already been done
+    </h1>
+    <p class="py-3 text-center text-lg">
+      If you would like to do it one more time, please press
+      <span class="font-bold">'I want to repeat the test'</span>
+    </p>
+    <custom-button text="I want to repeat the test" @click="restartData"></custom-button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import axios from 'axios';
+import CustomButton from '@/components/Shared/CustomButton.vue';
+
+function restartData() {
+  const baseUrl = import.meta.env.VITE_APP_API_URL;
+  const url = `${baseUrl}/answers`;
+  axios
+    .post(url, {})
+    .then((resp) => {
+      console.log(resp.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  window.location.reload();
+}
+
+defineExpose({ restartData });
+</script>
