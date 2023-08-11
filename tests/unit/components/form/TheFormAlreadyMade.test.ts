82e3015292caa54707be61c58c3b1e7ad93a0018
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-11 14:02:56 +0200

    test: add test for TheFormAlreadyMade.vue

---
+++ tests/unit/components/form/TheFormAlreadyMade.test.ts
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/vue';
+import TheFormAlreadyMade from '@/components/form/TheFormAlreadyMade.vue';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
+import type { Mock } from 'vitest';
+vi.mock('axios');
+const axiosPostMock = axios.post as Mock;
+const assignMock = vi.fn();
+
+describe('TheFormAlreadyMade', () => {
+  beforeEach(() => {
+    axiosPostMock.mockResolvedValue({});
+  });
+  it('displays h1 text', () => {
+    render(TheFormAlreadyMade);
+    const h1Text = screen.queryByText('It seems that the test has already been done');
+    expect(h1Text).toBeInTheDocument();
+  });
+
+  it('displays button', () => {
+    render(TheFormAlreadyMade);
+    const button = screen.queryByRole('button', { name: /i want to repeat the test/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('clears data on server', async () => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { reload: vi.fn() }
+    });
+    render(TheFormAlreadyMade);
+    const button = screen.queryByRole('button', {
+      name: /i want to repeat the test/i
+    }) as HTMLButtonElement;
+    await userEvent.click(button);
+    // window.location = { assign: assignMock as any } as Location;
+    expect(axios.post).toHaveBeenCalled();
+    expect(window.location.reload).toHaveBeenCalled();
+    Object.defineProperty(window, 'location', { configurable: true, value: window.location });
+  });
+});
