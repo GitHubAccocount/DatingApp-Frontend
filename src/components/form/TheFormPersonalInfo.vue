2369359293ecc485aa6a4fb4c8ee11a6bf82fbc6
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-12 14:49:41 +0200

    feat: render TheFormPersonalTest.vue

--- src/components/form/TheFormPersonalInfo.vue
+++ src/components/form/TheFormPersonalInfo.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>Hello there!</h1>
+  <section
+    class="mx-6 flex h-svh80 overflow-y-scroll rounded-md border bg-red-100 p-6 shadow-md shadow-gray-700"
+  >
+    <the-form-personal-test></the-form-personal-test>
+  </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import TheFormPersonalTest from './TheFormPersonalTest.vue';
+</script>
