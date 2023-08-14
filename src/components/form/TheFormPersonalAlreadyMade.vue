538eafda4fcad71c58d467c15b615ce8bed99af3
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-14 19:43:24 +0200

    feat: add new component which apperas when user tries to access the personal form before test or when perosnal form is done

---
+++ src/components/form/TheFormPersonalAlreadyMade.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex w-full flex-col items-center">
+    <h1 class="pb-3 text-center text-3xl font-bold">You can't access this page.</h1>
+    <p class="py-3 text-center text-lg">
+      If you would like to repeat the form, press <span class="font-bold"> the button below </span>.
+      It will <span class="font-bold"> redirect </span>you to place where from you can restart your
+      form.
+    </p>
+    <custom-button text="Redirect me" @click="redirect()"></custom-button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CustomButton from '../Shared/CustomButton.vue';
+import { useRouter } from 'vue-router';
+
+const redirect = () => {
+  const router = useRouter();
+  router.push({ path: '/form' });
+};
+</script>
