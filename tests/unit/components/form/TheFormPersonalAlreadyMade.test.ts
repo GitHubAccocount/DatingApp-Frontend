f4bcf96c0e29e2b8b5f7d0355ac7c7203062d1a4
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-14 20:17:13 +0200

    test: add test for TheFormPersonalAlreadyMade.vue

---
+++ tests/unit/components/form/TheFormPersonalAlreadyMade.test.ts
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/vue';
+import TheFormPersonalAlreadyMade from '@/components/form/TheFormPersonalAlreadyMade.vue';
+import type { Mock } from 'vitest';
+
+import { useRouter } from 'vue-router';
+import userEvent from '@testing-library/user-event';
+vi.mock('vue-router');
+const useRouterMock = useRouter as Mock;
+
+describe('TheFormPersonalAlreadyMade', () => {
+  it('displayes h1 text', () => {
+    render(TheFormPersonalAlreadyMade);
+    const h1Text = screen.queryByText("You can't access this page.");
+    expect(h1Text).toBeInTheDocument();
+  });
+
+  it('redirect user to /form page', async () => {
+    render(TheFormPersonalAlreadyMade);
+    const push = vi.fn();
+    useRouterMock.mockReturnValue({ push });
+
+    const button = screen.queryByRole('button', { name: /redirect me/i }) as HTMLButtonElement;
+    await userEvent.click(button);
+
+    expect(push).toHaveBeenCalledWith({ path: '/form' });
+  });
+});
