f077de97e734713ce254a2ebae5f98b8415988dc
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-15 22:05:38 +0200

    feat: render one of two componetn depending on conditions

--- src/components/form/TheFormPersonalInfo.vue
+++ src/components/form/TheFormPersonalInfo.vue
@@ -2,11 +2,19 @@
   <section
     class="mx-6 flex h-svh80 overflow-y-scroll rounded-md border bg-red-100 p-6 shadow-md shadow-gray-700"
   >
-    <the-form-personal-test></the-form-personal-test>
+    <the-form-personal-already-made
+      v-if="answers.length === 0 || personalInfo.length > 0"
+    ></the-form-personal-already-made>
+    <the-form-personal-test v-else></the-form-personal-test>
   </section>
 </template>
 
 <script lang="ts" setup>
 import TheFormPersonalTest from './TheFormPersonalTest.vue';
-import TheFormPersonalMadeAlready from './TheFormPersonalMadeAlready.vue';
+import TheFormPersonalAlreadyMade from './TheFormPersonalAlreadyMade.vue';
+import { useQuestionsStore } from '@/stores/questions';
+
+const useQuestions = useQuestionsStore();
+const personalInfo = useQuestions.personalInfo;
+const answers = useQuestions.answers;
 </script>
