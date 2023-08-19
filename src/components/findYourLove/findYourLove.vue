aa9c39c34c6412ba154be2a89a4433c5c7c52c43
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-19 07:23:31 +0200

    feat: add two components findYourLove.vue and findYourLoveBeforeForm.vue

---
+++ src/components/findYourLove/findYourLove.vue
@@ -0,0 +1,7 @@
+<template>
+  <find-your-love-before-form></find-your-love-before-form>
+</template>
+
+<script lang="ts" setup>
+import findYourLoveBeforeForm from '@/components/findYourLove/findYourLoveBeforeForm.vue';
+</script>
