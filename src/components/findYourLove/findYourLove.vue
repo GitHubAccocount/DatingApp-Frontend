bbaf8b5e112b2e1cb0541f9fab3122decef52643
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-19 07:50:07 +0200

    feat: import newly created component and render under condition of "v-else"

--- src/components/findYourLove/findYourLove.vue
+++ src/components/findYourLove/findYourLove.vue
@@ -1,7 +1,16 @@
 <template>
-  <find-your-love-before-form></find-your-love-before-form>
+  <find-your-love-before-form
+    v-if="personalInfo.length === 0 && answers.length === 0"
+  ></find-your-love-before-form>
+  <find-your-love-after-form v-else></find-your-love-after-form>
 </template>
 
 <script lang="ts" setup>
 import findYourLoveBeforeForm from '@/components/findYourLove/findYourLoveBeforeForm.vue';
+import findYourLoveAfterForm from '@/components/findYourLove/findYourLoveAfterForm.vue';
+import { useQuestionsStore } from '@/stores/questions';
+
+const useQuestions = useQuestionsStore();
+const personalInfo = useQuestions.personalInfo;
+const answers = useQuestions.answers;
 </script>
