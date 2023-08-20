ff96c4398da6ba0f754acee3f303194e8fc50b49
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-20 11:16:59 +0200

    feat: add action FETCH_USERS to fetch data and save it in constant "users"

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,5 +1,7 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
+import getUsers from '@/api/getUsers';
+import type { User } from '@/api/types';
 
 export const useUserStore = defineStore('user', () => {
   const showNav = ref(false);
@@ -7,5 +9,11 @@ export const useUserStore = defineStore('user', () => {
     return (showNav.value = !showNav.value);
   };
 
-  return { showNav, SHOW_NAV };
+  const users = ref<User[]>([]);
+  const FETCH_USERS = async () => {
+    const fetchUsers = await getUsers();
+    users.value = fetchUsers;
+  };
+
+  return { showNav, users, SHOW_NAV, FETCH_USERS };
 });
