f1f198aeec595f3dadcf26556eba7b8cd7c6ae7b
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-21 11:27:34 +0200

    feat: add infinite (actually finite) scroll to the component

--- src/components/findYourLove/findYourLoveAfterForm.vue
+++ src/components/findYourLove/findYourLoveAfterForm.vue
@@ -1,5 +1,70 @@
 <template>
-  <h1>After Form</h1>
+  <section>
+    <h1>Hello</h1>
+    <div class="grid grid-cols-2 gap-4" ref="scrollComponent">
+      <div
+        v-for="user in usersOnScroll"
+        :key="user.id"
+        class="rounded-md border border-red-400 shadow-md"
+      >
+        <router-link :to="'/find/' + user.id">
+          <div>
+            <img :src="user.profileImg" />
+          </div>
+          <div class="bg-red-400 p-2 font-bold text-white">
+            <p class="inline pr-1">{{ user.firstName }},</p>
+            <p class="inline">{{ user.age }}</p>
+            <p>{{ user.id }}</p>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useUserStore } from '@/stores/user';
+import { useQuestionsStore } from '@/stores/questions';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
+
+const userStore = useUserStore();
+onMounted(userStore.FETCH_USERS);
+
+const users = computed(() => {
+  return userStore.users;
+});
+
+const questionsStore = useQuestionsStore();
+onMounted(questionsStore.FETCH_PERSONAL_INFO);
+
+const personalInfo = computed(() => {
+  return questionsStore.personalInfo;
+});
+
+const scrollComponent = ref<HTMLElement | null>(null);
+let start = 0;
+let end = 10;
+const usersOnScroll = ref(users.value.slice(start, end));
+
+const loadMoreUsers = () => {
+  start += 10;
+  end += 10;
+  let newUsersOnScroll = users.value.slice(start, end);
+  usersOnScroll.value.push(...newUsersOnScroll);
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
+
+const handleScroll = () => {
+  const element = scrollComponent.value as HTMLElement;
+  if (element.getBoundingClientRect().bottom <= window.innerHeight + 10) {
+    loadMoreUsers();
+  }
+};
+</script>
