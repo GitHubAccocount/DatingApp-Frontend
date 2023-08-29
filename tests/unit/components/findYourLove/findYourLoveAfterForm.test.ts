d148f10840d0038138032b28b8997d72d2a68439
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-29 14:30:24 +0200

    test: apply utility function

--- tests/unit/components/findYourLove/findYourLoveAfterForm.test.ts
+++ tests/unit/components/findYourLove/findYourLoveAfterForm.test.ts
@@ -4,6 +4,7 @@ import findYourLoveAfterForm from '@/components/findYourLove/findYourLoveAfterFo
 import { useQuestionsStore } from '@/stores/questions';
 import { createTestingPinia } from '@pinia/testing';
 import { RouterLinkStub } from '@vue/test-utils';
+import { sampleUsers } from 'tests/utils/sampleUsers';
 
 describe('findYourLoveAfterForm', () => {
   const renderFindYourLoveAfterForm = () => {
@@ -28,34 +29,7 @@ describe('findYourLoveAfterForm', () => {
     questionsStore.personalInfo = [
       { gender: 'female', empathyLevel: 'low', lookingFor: 'female', description: 'aa' }
     ];
-    questionsStore.users = [
-      {
-        id: 1,
-        firstName: 'Jacklyn',
-        lastName: 'Lemke',
-        age: 100,
-        profileImg:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png',
-        ownedEmphatyLevel: 'low',
-        gender: 'female',
-        lookingFor: 'female',
-        empathyLevel: 'low',
-        description: 'H'
-      },
-      {
-        id: 2,
-        firstName: 'Jacklyn2',
-        lastName: 'Lemke2',
-        age: 102,
-        profileImg:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png',
-        ownedEmphatyLevel: 'low',
-        gender: 'male',
-        lookingFor: 'male',
-        empathyLevel: 'low',
-        description: 'H'
-      }
-    ];
+    questionsStore.users = sampleUsers();
 
     const routerLink = (await screen.findByAltText('user photo')) as HTMLElement;
     expect(routerLink).toBeInTheDocument();
