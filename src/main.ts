2ba8b5ecfbc09fb40444adfaba143f6b201b0aa1
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-31 11:39:35 +0200

    feat: add new icons

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faXmark, faBars } from "@fortawesome/free-solid-svg-icons";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faXmark,
+  faBars,
+  faChevronDown,
+  faChevronUp,
+  faFilter
+} from '@fortawesome/free-solid-svg-icons';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
-library.add(faXmark, faBars);
+library.add(faXmark, faBars, faChevronDown, faChevronUp, faFilter);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.component("font-awesome-icon", FontAwesomeIcon);
-app.mount("#app");
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
