de987815a376b75709d8fb04f1779c8455c44521
Adrian <aaadrianrutkaaa@o2.pl>
2023-08-31 11:38:22 +0200

    fix: change function to constant to aviod error during testing, when user is used as arguemnt in function, add type User

--- tests/utils/sampleUsers.ts
+++ tests/utils/sampleUsers.ts
@@ -1,30 +1,29 @@
-export const sampleUsers = () => {
-  return [
-    {
-      id: 1,
-      firstName: 'Jacklyn',
-      lastName: 'Lemke',
-      age: 100,
-      profileImg:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png',
-      ownedEmphatyLevel: 'low',
-      gender: 'female',
-      lookingFor: 'female',
-      empathyLevel: 'low',
-      description: 'H'
-    },
-    {
-      id: 2,
-      firstName: 'Jacklyn2',
-      lastName: 'Lemke2',
-      age: 102,
-      profileImg:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png',
-      ownedEmphatyLevel: 'low',
-      gender: 'male',
-      lookingFor: 'male',
-      empathyLevel: 'low',
-      description: 'H'
-    }
-  ];
-};
+import type { User } from '@/api/types';
+export const sampleUsers: User[] = [
+  {
+    id: 1,
+    firstName: 'Jacklyn',
+    lastName: 'Lemke',
+    age: 100,
+    profileImg:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png',
+    ownedEmphatyLevel: 'low',
+    gender: 'female',
+    lookingFor: 'female',
+    empathyLevel: 'low',
+    description: 'H'
+  },
+  {
+    id: 2,
+    firstName: 'Jacklyn2',
+    lastName: 'Lemke2',
+    age: 102,
+    profileImg:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png',
+    ownedEmphatyLevel: 'low',
+    gender: 'male',
+    lookingFor: 'male',
+    empathyLevel: 'low',
+    description: 'H'
+  }
+];
