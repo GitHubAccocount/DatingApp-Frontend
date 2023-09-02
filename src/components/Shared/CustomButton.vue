23ab22c3dcc2f1ee4f34f600d580ba8ed35de824
Adrian <aaadrianrutkaaa@o2.pl>
2023-09-02 10:34:17 +0200

    styles: add bigger font for bigger screens

--- src/components/Shared/CustomButton.vue
+++ src/components/Shared/CustomButton.vue
@@ -30,7 +30,7 @@ const props = defineProps({
 
 <style scoped>
 button {
-  @apply cursor-pointer rounded-md px-3 py-2 font-bold 2xl:border-2 2xl:px-6 2xl:py-4 2xl:text-2xl;
+  @apply cursor-pointer rounded-md px-3 py-2 font-bold sm:text-lg 2xl:border-2 2xl:px-6 2xl:py-4 2xl:text-2xl;
 }
 .loginButton {
   @apply border border-red-400 hover:bg-red-400 hover:text-rose-50;
