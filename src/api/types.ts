e8e3347085ebc1dd3fa5c38d0ef1969e00554edf
Adrian <aaadrianrutkaaa@o2.pl>
2023-09-04 08:03:02 +0200

    feat: add new type for blogs

--- src/api/types.ts
+++ src/api/types.ts
@@ -27,3 +27,23 @@ export interface User {
   empathyLevel: string;
   description: string;
 }
+
+export interface Blog {
+  id: number;
+  title: string;
+  date: string;
+  img: string;
+  subtitle1: string;
+  content1: string;
+  subtitle2: string;
+  content2: string;
+  subtitle3: string;
+  content3: string;
+  content4: string;
+  subtitle5: string;
+  content5: string;
+  subtitle6: string;
+  content6: string;
+  subtitle7: string;
+  content7: string;
+}
