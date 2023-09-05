df9af994684021ac687c40f489be79bd015fe144
Adrian <aaadrianrutkaaa@o2.pl>
2023-09-05 08:21:46 +0200

    test: add new test conditions according to changes in tested component

--- tests/unit/components/form/TheFormPersonalInfo.test.ts
+++ tests/unit/components/form/TheFormPersonalInfo.test.ts
@@ -16,29 +16,23 @@ describe('TheFormPersonalInfo', () => {
       }
     });
   };
-  it('renders the form already made when answers.length === 0 or personalInfo.length > 0', () => {
+  it('renders the form already made when answers is empty object or personalInfo.length > 0', () => {
     renderTheFormPersonalInfo();
     const questionsStore = useQuestionsStore();
-    const answers = (questionsStore.answers = [{ id: 1, selectedAnswer: '4' }]);
+    //@ts-ignore
+    const answers = (questionsStore.answers = {});
     const personalInfo = (questionsStore.personalInfo = [{ gender: 'F' }]);
 
-    const statement = answers.length === 0 || personalInfo.length > 0;
-    expect(statement).toBeTruthy();
-    // const h1Text = screen.queryByText("You can't access this page.");
-    // expect(h1Text).toBeInTheDocument();
+    const h1Text = screen.queryByText("You can't access this page.");
+    expect(h1Text).toBeInTheDocument();
   });
 
   it('renders the form personal test', async () => {
     renderTheFormPersonalInfo();
     const questionsStore = useQuestionsStore();
     const answers = (questionsStore.answers = [{ id: 1, selectedAnswer: '4' }]);
-    const personalInfo = (questionsStore.personalInfo = []);
 
-    const statement = answers.length === 0 || personalInfo.length > 0;
-    expect(statement).toBeFalsy();
-
-    // even though the conditions are met it doesnt render the correct component
-    // const h1Text = await screen.findByText('Additional Information');
-    // expect(h1Text).toBeInTheDocument();
+    const h1Text = await screen.findByText('Additional Information');
+    expect(h1Text).toBeInTheDocument();
   });
 });
