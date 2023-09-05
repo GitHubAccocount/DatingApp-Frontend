9b3c56057d94201de506e59afbe68826ed93f84b
Adrian <aaadrianrutkaaa@o2.pl>
2023-09-05 07:22:01 +0200

    feat: add footer which is rendered under specific conditions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <main-nav />
   <router-view />
+  <main-footer v-if="!shouldShowFooter" />
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import { RouterView, useRoute } from 'vue-router';
 import MainNav from '@/components/navigation/MainNav.vue';
+import MainFooter from './components/footer/MainFooter.vue';
+import { computed } from 'vue';
+
+const route = useRoute();
+// Function to check if the path matches the pattern "/find", "/form", or "/find/1", "/find/2", etc.
+const pathMatchesPattern = (path: string): boolean => {
+  const pattern = /^(\/find\/\d+|\/form\/\d+|\/find$|\/form$|\/form)/;
+  return pattern.test(path);
+};
+
+const shouldShowFooter = computed(() => {
+  const path = route.path;
+  return pathMatchesPattern(path);
+});
 </script>
 
 <style>
