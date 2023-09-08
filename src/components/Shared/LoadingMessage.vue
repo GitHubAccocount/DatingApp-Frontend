5f6ac5b23cb899a6193396e9a2c27b3041c2e106
Adrian <aaadrianrutkaaa@o2.pl>
2023-09-08 08:18:22 +0200

    feat: add new component which renders message for user during loading of content

---
+++ src/components/Shared/LoadingMessage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="classForLoading" id="loading-message">
+    <h2 class="text-2xl">Loading data...</h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  classForLoading: {
+    type: String,
+    required: false,
+    default: 'loading-message',
+    validator(value: string) {
+      return ['loading-message', 'loading-message-80', 'loading-message-full'].includes(value);
+    }
+  }
+});
+</script>
+
+<style scoped>
+.loading-message {
+  @apply flex items-center justify-center;
+}
+
+.loading-message-80 {
+  @apply h-svh80;
+}
+
+.loading-message-full {
+  @apply h-full;
+}
+</style>
